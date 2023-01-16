9aa9b8d13489cf3d2cbd3a37d64b3fcc2e541c34
Tristan Ross <tristan.ross@midstall.com>
2023-01-15 16:40:55 -0800

    fix: vendor hash

--- example/default.nix
+++ example/default.nix
@@ -1,7 +1,8 @@
-{ pkgs ? import <expidus-sdk> {} }:
+{ pkgs ? import <expidus-sdk> {}, lib ? pkgs.lib }:
+with lib;
 pkgs.expidus.buildPackage {
   pname = "expidus-runtime-example";
   inherit (pkgs.expidus.runtime) version;
-  src = ./.;
+  src = cleanSource "${pkgs.expidus.runtime.src}/example";
   vendorSha256 = "sha256-gK7WkV85AiLrVuB/SdzdRMVT8okTbGvkQdVkSbFDImE=";
 }
