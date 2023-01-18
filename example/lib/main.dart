623271c5eb9cda562615700576375a0753142dc3
tross <tross@ezoic.com>
2023-01-18 09:24:55 -0800

    fix(example): font rendering

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+ThemeData _buildTheme(brightness) {
+  var baseTheme = ThemeData(brightness: brightness);
+
+  return baseTheme.copyWith(
+    textTheme: GoogleFonts.latoTextTheme(baseTheme.textTheme),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -28,6 +37,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: _buildTheme(Brightness.dark),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
